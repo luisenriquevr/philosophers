55727064f6dabdce1f6e37f9f490fffca39ce8d9
Luis enrique Varela <lvarela@c1r10s5.42madrid.com>
2022-02-10 13:41:00 +0100

    holas printf deleted

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lvarela <lvarela@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 13:56:42 by lvarela           #+#    #+#             */
-/*   Updated: 2022/02/10 13:10:36 by lvarela          ###   ########.fr       */
+/*   Updated: 2022/02/10 13:39:51 by lvarela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,6 @@ int	parsing(t_data *data, int argc, char **argv)
 			return (1);
 	}
 	if (argc == 5)
-	{
-		printf("hola");
 		data->parameters[NUM_OF_TIMES_TO_EAT] = -1;
-	}
 	return (0);
 }
